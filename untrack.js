945b6385948e1ef1feb1879535d34e261d68104a
Robert Wertman <robert.wertman@gmail.com>
2012-02-23 12:41:23 -0500

    Updated to use events. Much more efficient.

--- untrack.js
+++ untrack.js
@@ -1,30 +1,20 @@
-var old_length = 0;
-var container = document.getElementById("home_stream");
-var selector = "a[onmousedown^='UntrustedLink.bootstrap']";
-function fixLinks() {
-	// If we have a container.
-	if (container) {
-		// If our content hasn't been updated.
-		if (container.childNodes.length === old_length) {
-			// Redelay and return.
-			delayFixLinks();
-			return;
-		} else {
-			// Update our length and continue.
-			old_length = container.childNodes.length;
-		}
+var containers = ["contentCol", "fbDockChatTabs"];
+
+window.addEventListener("load", function() {
+	for (var i = 0; i < containers.length; ++i) {
+		document.getElementById(containers[i]).addEventListener("DOMNodeInserted", fixLinks);
 	}
 
-	// Process the links.
-	var aList = document.querySelectorAll(selector);
+	containers = undefined;
+}, false);
+
+var selector = "a[onmousedown^='UntrustedLink.bootstrap']";
+
+function fixLinks(e) {
+	var aList = e.relatedNode.querySelectorAll(selector);
 	for (var i = 0, len = aList.length; i < len; ++i) {
 		aList[i].removeAttribute('onmousedown');
 	}
+}
 
-	// Run again soon.
-	delayFixLinks();
-}
-function delayFixLinks() {
-	window.setTimeout(fixLinks, 400);
-}
-fixLinks();
+fixLinks({relatedNode: document});

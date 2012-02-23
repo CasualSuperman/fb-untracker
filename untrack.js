1731a743177929f6d84ca57a46e03e14287a56c9
Robert Wertman <robert.wertman@gmail.com>
2012-02-22 20:15:50 -0500

    Updated to be less expensive if we didn't scroll.

--- untrack.js
+++ untrack.js
@@ -5,14 +5,34 @@ function toArr(collection) {
 	}
 	return list;
 }
-
+var old_length = 0;
+var container = document.getElementById("home_stream");
 var selector = "a[onmousedown^='UntrustedLink.bootstrap']";
 function fixLinks() {
+	// If we have a container.
+	if (container) {
+		// If our content hasn't been updated.
+		if (container.childNodes.length === old_length) {
+			// Redelay and return.
+			delayFixLinks();
+			return;
+		} else {
+			// Update our length and continue.
+			old_length = container.childNodes.length;
+		}
+	}
+
+	// Process the links.
 	var aList = document.querySelectorAll(selector);
 	var elems = toArr(aList);
 	elems.forEach(function(elem) {
 		elem.onmousedown = undefined;
 	});
+
+	// Run again soon.
+	delayFixLinks();
+}
+function delayFixLinks() {
 	window.setTimeout(fixLinks, 400);
 }
 fixLinks();
